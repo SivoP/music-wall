75a9954f12746389fc1fcce91d6fb52d0fc7d106
SivoP <siv.padhy@gmail.com>
2015-07-14 18:43:01 +0000

    initial commit

---
+++ db/migrate/1_create_tables.rb
@@ -0,0 +1,10 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :tracks do |t|
+      t.string :song_title
+      t.string :author
+      t.string :url
+      t.timestamps
+    end
+  end
+end
