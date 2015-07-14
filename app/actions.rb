57bdc1b184cb32386db37c7cb6d16275aa1e41d4
SivoP <siv.padhy@gmail.com>
2015-07-14 20:40:53 +0000

    got forms working

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,26 @@
 get '/' do
   erb :index
 end
+
+get '/tracks/?' do
+ @tracks = Track.all
+ erb :'tracks'
+end
+
+get '/tracks/new' do
+  erb :'new_track'
+end
+
+
+post '/tracks' do
+  # binding.pry
+  @track = Track.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url: params[:url])
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'new_track'
+  end 
+end
